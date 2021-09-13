8ea20009e4b8f98b50ec690a2c82345ed2a42fe5
Megan McComb <megan.mccomb@utexas.edu>
2021-09-13 19:31:21 -0400

    begin hw3

---
+++ homework3.R
@@ -0,0 +1,12 @@
+# Homework #2 -------------------------------------------------------------
+
+
+#Load Libraries 
+library(tidyverse)
+library(quanteda)
+library(quanteda.textstats)
+
+
+#Load Data
+data <- read_csv("gop_debates.csv")
+
