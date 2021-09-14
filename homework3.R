0acf015725343784a4d133887c03a0d9210f728d
Megan McComb <megan.mccomb@utexas.edu>
2021-09-13 20:02:24 -0400

    updated hw3

--- homework3.R
+++ homework3.R
@@ -10,3 +10,24 @@ library(quanteda.textstats)
 #Load Data
 data <- read_csv("gop_debates.csv")
 
+# ?textstat_readability
+
+#print the text with who said it
+data$text
+
+#measure the readiblity of every line of text
+textstat_readability(data$text, measure = "Flesch.Kincaid")
+
+#calculate lexical diversity
+textstat_lexdiv(
+  data$text,
+  measure = c("TTR", "C", "R", "CTTR", "U", "S", "K", "I", "D", "Vm", "Maas", "MATTR",
+              "MSTTR", "all"),
+  remove_numbers = TRUE,
+  remove_punct = TRUE,
+  remove_symbols = TRUE,
+  remove_hyphens = FALSE,
+  log.base = 10,
+  MATTR_window = 100L,
+  MSTTR_segment = 100L,
+)
