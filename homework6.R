70baa73ade353dc177dd67ec1d5cdba1efd87543
Megan McComb <megan.mccomb@utexas.edu>
2021-10-03 18:56:48 -0400

    final commit

--- homework6.R
+++ homework6.R
@@ -21,9 +21,6 @@ rubio <- filter(data, who == "RUBIO")
 
 # Framegrams --------------------------------------------------------------
 
-#stringr::str_detect()
-?str_detect()
-
 # Stem and lemmatize with synonyms
 stem_strings("immigration displacement migration")
 lemmatize_strings("immigration displacement migration")
@@ -31,23 +28,7 @@ lemmatize_strings("immigration displacement migration")
 # Save terms for later
 immig_terms <- "immigration|displacement|migration|immigr|displac|migrat"
 
-# Immigration framegram (with added terms)
-data %>%
-  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
-  #anti_join(stop_words) %>%
-  count(trigram, sort = TRUE) %>%
-  filter(str_detect(trigram,immig_terms)) %>% 
-  filter(n > 2) %>% # note filter change 
-  mutate(trigram = reorder(trigram, n)) %>%
-  ggplot(aes(x=trigram, y=n)) +
-  geom_col() +
-  xlab(NULL) +
-  coord_flip()
-
 # Addressing Stop Words ----------------------------------------------------
-
-#stringr::str_count()
-?str_count()
 
 #paste
 paste(stop_words$word, collapse = "|")
@@ -56,16 +37,56 @@ paste(stop_words$word, collapse = "|")
 stop_words_bounded <- paste0("\\b", stop_words$word, "\\b", collapse = "|")
 
 
-# Top 15 Economic terms framegram (remove double stopwords) 
-data %>%
+# Top 10 immigration terms framegram for Trump (remove double stopwords) 
+trump %>%
   unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
   #anti_join(stop_words) %>%
   count(trigram, sort = TRUE) %>%
   filter(str_detect(trigram,immig_terms)) %>% 
   filter(str_count(trigram,stop_words_bounded) < 1) %>% 
   mutate(trigram = reorder(trigram, n)) %>%
-  slice(1:15) %>% # top 10 
+  slice(1:10) %>% # top 10 
   ggplot(aes(x=trigram, y=n)) +
   geom_col() +
   xlab(NULL) +
   coord_flip()
+
+# Top 10 immigration terms framegram for Cruz (remove double stopwords) 
+cruz %>%
+  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
+  #anti_join(stop_words) %>%
+  count(trigram, sort = TRUE) %>%
+  filter(str_detect(trigram,immig_terms)) %>% 
+  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
+  mutate(trigram = reorder(trigram, n)) %>%
+  slice(1:10) %>% # top 10 
+  ggplot(aes(x=trigram, y=n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+# Top 10 immigration terms framegram for Rubio (remove double stopwords) 
+rubio %>%
+  unnest_tokens(trigram, text, token = "ngrams", n=3) %>% 
+  #anti_join(stop_words) %>%
+  count(trigram, sort = TRUE) %>%
+  filter(str_detect(trigram,immig_terms)) %>% 
+  filter(str_count(trigram,stop_words_bounded) < 1) %>% 
+  mutate(trigram = reorder(trigram, n)) %>%
+  slice(1:10) %>% # top 10 
+  ggplot(aes(x=trigram, y=n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+# Conclusion -------------------------------------------------------------
+
+# We can tell based on the trigrames that Trump, Cruz, and Rubio have different ideas
+# of immigration. Trump uses phrases like "stop illegal immigration" and "horribly
+# illegal immigration" to show his negative thoughts on immigration. Cruz uses phrases 
+# like "hiring illegal immigrants" and "embrace legal immigrants" to show his accepting 
+# view on immigration. Rubio uses phrases like "celebrate legal immigration" and "allowing
+# undocumented immigrants" to show his acceptance of immigrants as well. The most salient
+# differences in how each spoke was that Trump spoke about them more negatively and Cruz
+# and Rubio spoke about immigrants more positively/ in a welcoming manner.
