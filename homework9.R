dbc6d0fe6f0a6aa434184f77395d33ae9ac71bf2
Megan McComb <megan.mccomb@utexas.edu>
2021-11-08 13:49:01 -1000

    first commit

---
+++ homework9.R
@@ -0,0 +1,29 @@
+# Homework #9  ------------------------------------
+
+# This R script develops a Spam detection AI for the spam_ham.CSV
+
+
+# Pre-Flight --------------------------------------------------------------
+
+#load libraries 
+library(tidyverse)
+library(tidytext)
+library(stringr)
+library(textstem)
+library(irr)
+
+#load data
+data <- read_csv("spam_ham.csv")
+
+
+#Analysis -----------------------------------------------------------------
+
+# Auto-code for call to action Spam words
+cta <- data %>%
+  mutate(Message = tolower(Message),
+         cta_spam = ifelse(str_detect(Message,"text|txt|claim|call|collect|join|click|win|go to|send"),1,0))
+
+spammsg <- cta$cta_spam
+
+
+
